2404ccba37faf3998a68ffb472702a3c44aed632
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2024-11-18 00:16:23 +0600

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import "./App.css";
-import Footer from "./pages/Shared/Footer/Footer";
-import Navbar from "./pages/Shared/NavBar/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
